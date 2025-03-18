6123b3affb4134c007a23bfb9f0a84ce9197b385
Aylin Rodriguez <aylin.rodriguez@magayacatapult.com>
2025-03-18 12:52:44 -0400

    Update TC4

--- Script/VerifyBaseRate.js
+++ Script/VerifyBaseRate.js
@@ -6,13 +6,15 @@ function VerifyBaseRate(margin) {
   // Perform the addition
   var actualResult = Number(Project.Variables.new_baseRate);
   var expectedResult = margin + originalBR;
+  var decimalExpected = parseFloat(expectedResult.toFixed(2));
+  var decimalActual = parseFloat(actualResult.toFixed(2));
 
   // Verify the result
-  if (actualResult == expectedResult) {
-    Log.Message("The result is correct: " + actualResult);
+  if (decimalActual == decimalExpected) {
+    Log.Message("The result is correct: " + decimalActual);
     return true;
   } else {
-    Log.Error("The result is incorrect. Expected: " + expectedResult + ", but got: " + actualResult);
+    Log.Error("The result is incorrect. Expected: " + decimalExpected + ", but got: " + decimalActual);
     return false;
   }
 }
